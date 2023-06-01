1fa8d8c68016e90de9856ca2c5da2a3588a9cf1c
ifkreis <mikaelmpr@gmail.com>
2023-06-01 11:52:06 -0300

    implementado algumas interações no main

--- main.js
+++ main.js
@@ -2,9 +2,16 @@
 
 
 
-
-
 function Exibir_Informacoes(){
     const Popup = document.querySelector(".Popup");
     Popup.style.display === "none" ? Popup.style.display = "none" : Popup.style.display = "flex";
+    let Nome_Imprimir=document.querySelector('.Nome_Aluno_Popup');
+
+
+    
+    let Nome = 'Mikael';
+    Nome_Imprimir.textContent = Nome;
+    let Curso = 'LCC';
+    let Ano_Ingresso = '2021.1'
+    let Infomacoes = 'informaçoes sobre o aluno irá aparecer aqui';
 }
