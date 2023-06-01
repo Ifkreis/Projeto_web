f68559e4cec2487d2a7f2a2d8ebb282145d40880
ifkreis <mikaelmpr@gmail.com>
2023-06-01 10:20:45 -0300

    01-06

---
+++ main.js
@@ -0,0 +1,10 @@
+
+
+
+
+
+
+function Exibir_Informacoes(){
+    const Popup = document.querySelector(".Popup");
+    Popup.style.display === "none" ? Popup.style.display = "none" : Popup.style.display = "flex";
+}
